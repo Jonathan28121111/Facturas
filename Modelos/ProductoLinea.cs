e5479ba548e418f5b376d4a3a8a128ee29cec32f
Jonathan28121111 <jonathan120428@gmail.com>
2025-11-05 13:40:38 -0700

    agregar clase ProductoLinea

---
+++ Modelos/ProductoLinea.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaFacturas.Modelos
+{
+    public class ProductoLinea
+    {
+        public string NombreProducto { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public int CantidadProducto { get; set; } = 1;
+
+        public decimal ImporteLinea => PrecioUnitario * CantidadProducto;
+    }
+}
