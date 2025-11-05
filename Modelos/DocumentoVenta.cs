08e402b62f826bc9d6e2a3ce8a00824818bf49a4
Jonathan28121111 <jonathan120428@gmail.com>
2025-11-05 13:43:02 -0700

    agregue clase DocumentoVenta

---
+++ Modelos/DocumentoVenta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaFacturas.Modelos
+{
+    public class DocumentoVenta
+    {
+        public int NumeroDocumento { get; set; }
+        public DateTime FechaEmision { get; set; }
+        public string NombreReceptor { get; set; }
+        public List<ProductoLinea> LineasDetalle { get; set; } = new List<ProductoLinea>();
+    }
+}
