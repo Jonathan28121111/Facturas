7fb3af59f7e5aca46e65517406a017a62115ee8c
Jonathan28121111 <jonathan120428@gmail.com>
2025-11-09 16:43:36 -0700

    Al igual con el documento venta le agregue el ENTITY FRAMEWORK

--- Modelos/ProductoLinea.cs
+++ Modelos/ProductoLinea.cs
@@ -1,11 +1,19 @@
-﻿namespace SistemaFacturas.Modelos
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaFacturas.Modelos
 {
     public class ProductoLinea
     {
-        public string NombreProducto { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int DocumentoId { get; set; }
+        public string NombreProducto { get; set; } = "";
         public decimal PrecioUnitario { get; set; }
         public int CantidadProducto { get; set; } = 1;
 
+        [NotMapped]
         public decimal ImporteLinea => PrecioUnitario * CantidadProducto;
     }
 }
