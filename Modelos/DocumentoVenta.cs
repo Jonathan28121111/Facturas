c659f0eda7b3649c763d68948ceea6e3fbcc2356
Jonathan28121111 <jonathan120428@gmail.com>
2025-11-10 13:42:46 -0700

    Agregar modelos: DocumentoVenta y ProductoLinea

---
+++ Modelos/DocumentoVenta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace SistemaFacturas.Modelos
+{
+    public class DocumentoVenta
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int NumeroDocumento { get; set; }
+
+        public DateTime FechaEmision { get; set; } = DateTime.Now;
+
+        [Required]
+        public string NombreReceptor { get; set; } = string.Empty;
+[Required]
+        public List<ProductoLinea> LineasDetalle { get; set; } = new List<ProductoLinea>();
+
+        [NotMapped]
+        public decimal ImporteTotal => LineasDetalle?.Sum(l => l.ImporteLinea) ?? 0m;
+    }
+}
---
+++ Modelos/ProductoLinea.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaFacturas.Modelos
+{
+    public class ProductoLinea
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int DocumentoId { get; set; }
+
+        [Required]
+        public string NombreProducto { get; set; } = string.Empty;
+
+        [Range(0, double.MaxValue)]
+        public decimal PrecioUnitario { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int CantidadProducto { get; set; } = 1;
+
+        [NotMapped]
+        public decimal ImporteLinea => PrecioUnitario * CantidadProducto;
+    }
+}
