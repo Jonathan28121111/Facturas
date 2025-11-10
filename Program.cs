df498022d8370ce979985dd9ded8de21347631cd
Jonathan28121111 <jonathan120428@gmail.com>
2025-11-09 17:27:20 -0700

    Agregamos los paquetes necesarios en program.cs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,31 @@
 using Facturas.Components;
+using Microsoft.EntityFrameworkCore;
+using SistemaFacturas.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddDbContext<FacturasDbContext>(options =>
+    options.UseSqlite("Data Source=facturas.db"));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<FacturasDbContext>();
+    db.Database.EnsureCreated();
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
