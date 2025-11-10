c5422c9abec64154b8306338e6744dd565012293
Jonathan28121111 <jonathan120428@gmail.com>
2025-11-09 17:26:22 -0700

    Se creo la carpeta data y dentro de ella hicimos el elemnto de facturas para que funcione con la base de dato SQLite

---
+++ FacturasDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaFacturas.Modelos;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace SistemaFacturas.Data
+{
+    public class FacturasDbContext : DbContext
+    {
+        public FacturasDbContext(DbContextOptions<FacturasDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<DocumentoVenta> Documentos { get; set; }
+        public DbSet<ProductoLinea> Lineas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DocumentoVenta>()
+                .HasKey(d => d.NumeroDocumento);
+
+            modelBuilder.Entity<ProductoLinea>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<DocumentoVenta>()
+                .HasMany(d => d.LineasDetalle)
+                .WithOne()
+                .HasForeignKey(l => l.DocumentoId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
